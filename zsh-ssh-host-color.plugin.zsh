bc137040b64d6a87a5985f5996456c3e3a3a21f2
Shayne Holmes <shayneh@qualtrics.com>
2018-03-28 09:28:14 -0700

    Support both ssh and sshrc
    Also fix pattern matching for hostnames to include spaces around the
    name as well as dots.

--- zsh-ssh-host-color.plugin.zsh
+++ zsh-ssh-host-color.plugin.zsh
@@ -1,31 +1,36 @@
-#!/bin/bash
-#
-# (1) copy to: ~/bin/ssh-host-color 
-# (2) set:     alias ssh=~/bin/ssh-host-color
-#
 # Inspired from http://talkfast.org/2011/01/10/ssh-host-color
 # Fork from https://gist.github.com/773849
-#
+
 
 function _ssh_host_color() (
   local oldcolor
   oldcolor=$_iterm2colors_current
-  if [[ "$*" =~ '(^|\.)(engserv-co1|co1|iad1|ca1|az1|eu2)($|\.)' ]]; then
+  if [[ "$*" =~ '(^| |\.)(engserv-co1|co1|iad1|ca1|az1|eu2)($| |\.)' ]]; then
     _iterm2colors_apply Homebrew
-  elif [[ "$*" =~ '(^|\.)(b1-prv|st3)($|\.)' ]]; then
+  elif [[ "$*" =~ '(^| |\.)(b1|b1-prv|st3)($| |\.)' ]]; then
     _iterm2colors_apply Earthsong
   fi
   trap "_iterm2colors_apply '$oldcolor'" INT
   trap "_iterm2colors_apply '$oldcolor'" EXIT
-  sshrc "$@"
+  $@
   _iterm2colors_apply "$oldcolor"
 )
 
-function _ssh_host_color_complete() {
+function _ssh_color() {
+  _ssh_host_color ssh $@
+}
+
+function _sshrc_color() {
+  _ssh_host_color sshrc $@
+}
+
+function _ssh_complete() {
   local service=ssh
   _ssh "$@"
 }
 
-compdef _ssh_host_color_complete _ssh_host_color
+compdef _ssh_complete _ssh_color
+compdef _ssh_complete _sshrc_color
 
-alias ssh=_ssh_host_color
+alias ssh="_ssh_color"
+alias sshrc="_sshrc_color"
