9e2f4669a5e4e0f57f4f8f0d495c7a63136df1af
Shayne Holmes <shayneh@qualtrics.com>
2018-03-30 14:12:56 -0700

    Fix coloring so it works with generic things

--- zsh-ssh-host-color.plugin.zsh
+++ zsh-ssh-host-color.plugin.zsh
@@ -1,37 +1,19 @@
 # Inspired from http://talkfast.org/2011/01/10/ssh-host-color
 # Fork from https://gist.github.com/773849
 
-
-_ssh_host_color() {
+_color_from_params() {
+  setopt aliases
   emulate -L zsh # so traps don't percolate up
   local oldcolor
   oldcolor=$_iterm2colors_current
-  if [[ "$*" =~ '(^| |\.)(engserv-co1|co1|iad1|ca1|az1|eu2)($| |\.)' ]]; then
+  if [[ "$@" =~ '(^| |\.)(engserv-co1|au1|iad1|ca1|az1|eu2)($| |\.)' ]]; then
     _iterm2colors_apply Homebrew
-  elif [[ "$*" =~ '(^| |\.)(b1|b1-prv|st3)($| |\.)' ]]; then
+  elif [[ "$@" =~ '(^| |\.)(b1|b1-prv|st3)($| |\.)' ]]; then
     _iterm2colors_apply Earthsong
   fi
   trap "_iterm2colors_apply '$oldcolor'" INT
   trap "_iterm2colors_apply '$oldcolor'" EXIT
-  $@
+  "$@"
   _iterm2colors_apply "$oldcolor"
 }
 
-_ssh_color() {
-  _ssh_host_color ssh $@
-}
-
-_sshrc_color() {
-  _ssh_host_color sshrc $@
-}
-
-_ssh_complete() {
-  local service=ssh
-  _ssh "$@"
-}
-
-compdef _ssh_complete _ssh_color
-compdef _ssh_complete _sshrc_color
-
-alias ssh="_ssh_color"
-alias sshrc="_sshrc_color"
