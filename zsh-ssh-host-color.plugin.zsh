8598030bde3ec2b7878d2dc9d9b62e228685d42b
Shayne Holmes <shayneh@qualtrics.com>
2018-04-02 08:24:07 -0700

    Set colors and patterns in an array
    Becomes a little more easily extensible. Also, don't apply the old color
    unless we change it.

--- zsh-ssh-host-color.plugin.zsh
+++ zsh-ssh-host-color.plugin.zsh
@@ -1,19 +1,27 @@
 # Inspired from http://talkfast.org/2011/01/10/ssh-host-color
 # Fork from https://gist.github.com/773849
 
+typeset -xA _color_from_params_patterns
+
+_color_from_params_patterns+=("Homebrew" '(^| |\.)(engserv-co1|au1|iad1|ca1|az1|eu2)($| |\.)')
+_color_from_params_patterns+=("Earthsong" '(^| |\.)(b1|b1-prv|st3)($| |\.)')
+
 _color_from_params() {
   setopt aliases
   emulate -L zsh # so traps don't percolate up
   local oldcolor
-  oldcolor=$_iterm2colors_current
-  if [[ "$@" =~ '(^| |\.)(engserv-co1|au1|iad1|ca1|az1|eu2)($| |\.)' ]]; then
-    _iterm2colors_apply Homebrew
-  elif [[ "$@" =~ '(^| |\.)(b1|b1-prv|st3)($| |\.)' ]]; then
-    _iterm2colors_apply Earthsong
-  fi
-  trap "_iterm2colors_apply '$oldcolor'" INT
-  trap "_iterm2colors_apply '$oldcolor'" EXIT
+  for color pattern in ${(kv)_color_from_params_patterns}; do
+    if [[ "$@" =~ "$pattern" ]]; then
+      oldcolor=$_iterm2colors_current
+      _iterm2colors_apply "$color"
+      trap "_iterm2colors_apply '$oldcolor'" INT
+      trap "_iterm2colors_apply '$oldcolor'" EXIT
+      break
+    fi
+  done
   "$@"
-  _iterm2colors_apply "$oldcolor"
+  if [ "$oldcolor" ]; then
+    _iterm2colors_apply "$oldcolor"
+  fi
 }
 
