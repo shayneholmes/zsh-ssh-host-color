5e32499ed4fb2228e3f727c0edf6d38723d400ff
Shayne Holmes <shayneh@qualtrics.com>
2018-09-07 07:29:56 -0700

    Provide colorscheme per environment
    A new layer of abstraction!

--- zsh-ssh-host-color.plugin.zsh
+++ zsh-ssh-host-color.plugin.zsh
@@ -1,18 +1,25 @@
 # Inspired from http://talkfast.org/2011/01/10/ssh-host-color
 # Fork from https://gist.github.com/773849
 
-typeset -xA _color_from_params_patterns
+typeset -xA _environment_pattern
+typeset -xA _color_from_environment
 
-_color_from_params_patterns+=("Homebrew" '(^| |\.)(engserv-co1|au1|iad1|ca1|az1|eu2)($| |\.)')
-_color_from_params_patterns+=("Earthsong" '(^| |\.)(b1|b1-prv|st3)($| |\.)')
+_environment_pattern[prod]='(^| |\.)(au1|az1|ca1|eu2|iad1|sjc1|syd1|gov1|va2|eng)($| |\.)'
+_environment_pattern[stage]='(^| |\.)(g1|g1-iad)($| |\.)'
+_environment_pattern[dev]='(^| |\.)(a1|a1-prv|b1|b1-prv)($| |\.)'
+
+_color_from_environment[prod]="Homebrew"
+_color_from_environment[stage]="Earthsong"
+_color_from_environment[dev]="Earthsong"
 
 _color_from_params() {
   setopt aliases
   emulate -L zsh # so traps don't percolate up
   local oldcolor
-  for color pattern in ${(kv)_color_from_params_patterns}; do
+  for environment pattern in ${(kv)_environment_pattern}; do
     if [[ "$@" =~ "$pattern" ]]; then
       oldcolor=$_iterm2colors_current
+      color=$_color_from_environment[$environment]
       _iterm2colors_apply "$color"
       trap "_iterm2colors_apply '$oldcolor'" INT
       trap "_iterm2colors_apply '$oldcolor'" EXIT
@@ -24,4 +31,3 @@ _color_from_params() {
     _iterm2colors_apply "$oldcolor"
   fi
 }
-
