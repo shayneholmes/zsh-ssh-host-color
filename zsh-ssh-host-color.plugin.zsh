31589ee328b618ea399d89a7854156725d644073
Shayne Holmes <shayneh@qualtrics.com>
2018-09-07 10:07:28 -0700

    Break DC declaration apart from pattern generation

--- zsh-ssh-host-color.plugin.zsh
+++ zsh-ssh-host-color.plugin.zsh
@@ -1,16 +1,45 @@
 # Inspired from http://talkfast.org/2011/01/10/ssh-host-color
 # Fork from https://gist.github.com/773849
 
+_prod_dcs=(
+  au1
+  az1
+  ca1
+  eng
+  eu2
+  gov1
+  iad1
+  sjc1
+  syd1
+  va2
+)
+
+_stage_dcs=(
+  g1
+  g1-iad
+)
+
+_dev_dcs=(
+  a1
+  a1-prv
+  b1
+  b1-prv
+)
+
+typeset -xA _colorschemes
+_colorschemes[prod]="Homebrew"
+_colorschemes[stage]="AdventureTime"
+_colorschemes[dev]="Earthsong"
+
+_get_pattern_from_dcs() {
+  _pattern=$(printf "$@" | sed "s/ /|/g")
+  printf '(^| |\.)(%s)($| |\.)' "$_pattern"
+}
+
 typeset -xA _environment_pattern
-typeset -xA _color_from_environment
-
-_environment_pattern[prod]='(^| |\.)(au1|az1|ca1|eu2|iad1|sjc1|syd1|gov1|va2|eng)($| |\.)'
-_environment_pattern[stage]='(^| |\.)(g1|g1-iad)($| |\.)'
-_environment_pattern[dev]='(^| |\.)(a1|a1-prv|b1|b1-prv)($| |\.)'
-
-_color_from_environment[prod]="Homebrew"
-_color_from_environment[stage]="AdventureTime"
-_color_from_environment[dev]="Earthsong"
+_environment_pattern[prod]=$(_get_pattern_from_dcs "$_prod_dcs")
+_environment_pattern[stage]=$(_get_pattern_from_dcs "$_stage_dcs")
+_environment_pattern[dev]=$(_get_pattern_from_dcs "$_dev_dcs")
 
 _color_from_params() {
   setopt aliases
@@ -19,7 +48,7 @@ _color_from_params() {
   for environment pattern in ${(kv)_environment_pattern}; do
     if [[ "$@" =~ "$pattern" ]]; then
       oldcolor=$_iterm2colors_current
-      color=$_color_from_environment[$environment]
+      color=$_colorschemes[$environment]
       _iterm2colors_apply "$color"
       trap "_iterm2colors_apply '$oldcolor'" INT
       trap "_iterm2colors_apply '$oldcolor'" EXIT
